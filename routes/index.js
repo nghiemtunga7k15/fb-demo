a934d0a29ac266ee7ff68f927e8d5b95d8e471d0
Your Name <you@example.com>
2021-03-11 17:10:59 +0700

    dsasad

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log(req.query.code)
+	console.log(req.query.state)
   res.render('index', { title: 'Express' });
+  // https://graph.facebook.com/v3.1/ads_archive?access_tokenE=EAABvoAZAq3x4BANvZBBab16kJvLrTCJyRb3b8EFrAnhcHHLZAESZCCWySgMnbescSMNhhmSDdDsQM7rZAJuALrXGzn4sZB5aPVmwrRl3GtWGJaEZB8ll2JW1GKq2FknvSj6ZB8FORElrHqFKWZAuvHcdnlUuSvT8aGQcVZBetFLUuX7oRHWd6v4ZAW8eOzpQliAl5ib4Yfwd9nSvgZDZD&search_terms='california'&ad_reached_countries=['vn']&ad_type=ALL
+
 });
 
 module.exports = router;
